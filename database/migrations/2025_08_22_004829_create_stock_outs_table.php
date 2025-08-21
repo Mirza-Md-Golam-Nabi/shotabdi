20a0d9324fca278b37a7a26989ea5fe685c55511
Nabi <g.nabi@shadhinbd.com>
2025-08-22 00:55:10 +0600

    Added Stockout model and migration file

--- database/migrations/2025_08_22_004829_create_stock_outs_table.php
+++ database/migrations/2025_08_22_004829_create_stock_outs_table.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Enums\AvailableEnum;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stock_ins', function (Blueprint $table) {
+        Schema::create('stock_outs', function (Blueprint $table) {
             $table->id();
             $table->date('date');
-            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->unsignedBigInteger('customer_id');
             $table->unsignedSmallInteger('product_id');
             $table->unsignedSmallInteger('quantity');
             $table->decimal('rate', 8, 2);
             $table->unsignedSmallInteger('discount')->nullable()->default(0);
             $table->decimal('amount', 12, 2);
-            $table->unsignedTinyInteger('is_available')->default(AvailableEnum::INACTIVE->value)->nullable();
             $table->timestamps();
             $table->softDeletes();
             $table->index('date');
@@ -35,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('stock_ins');
+        Schema::dropIfExists('stock_outs');
     }
 };
