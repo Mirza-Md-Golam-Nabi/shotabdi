86ecd699b1b9a4a6b47ece625aa92a9f8c1b682c
Nabi <g.nabi@shadhinbd.com>
2026-02-16 19:22:45 +0600

    Temp Added a code

--- app/Filament/Resources/StockOutResource/Pages/CreateStockOut.php
+++ app/Filament/Resources/StockOutResource/Pages/CreateStockOut.php
@@ -30,8 +30,8 @@ class CreateStockOut extends CreateRecord
     protected function handleRecordCreation(array $data): Model
     {
         session([
-            'last_customer_id' => $data['stock_ins'][0]['customer_id'] ?? null,
-            'last_product_id' => $data['stock_ins'][0]['product_id'] ?? null,
+            'last_customer_id' => $data['stock_outs'][0]['customer_id'] ?? null,
+            'last_product_id' => $data['stock_outs'][0]['product_id'] ?? null,
         ]);
         $this->date = $data['date'];
 
